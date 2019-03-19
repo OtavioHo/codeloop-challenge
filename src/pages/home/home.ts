44ce793329fce9df567a721c9576135c74882454
Otavio <otavio.aragoni@gmail.com>
2019-03-19 11:47:39 -0300

    Add method to open new modal

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, ModalController } from "ionic-angular";
+
+import { AddModal } from "../add-modal/add-modal";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
   students: Array<string>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public modalCtrl: ModalController
+  ) {
+    this.students = ["otavio", "pedro", "rafael", "henrique"];
+  }
 
-    this.students = [
-      'otavio',
-      'pedro',
-      'rafael',
-      'henrique'
-    ]
-
+  openAddModal() {
+    let addModal = this.modalCtrl.create(AddModal);
+    addModal.present();
   }
 }
