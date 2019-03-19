7146cf65cc2df3e95fe133a083a312c1fe1cfc03
Otavio <otavio.aragoni@gmail.com>
2019-03-19 15:45:48 -0300

    Import IonicStorageModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
+import { IonicStorageModule } from "@ionic/storage";
 
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
@@ -11,7 +12,11 @@ import { StudentPage } from "../pages/student/student";
 
 @NgModule({
   declarations: [MyApp, HomePage, AddModal, StudentPage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, HomePage, AddModal, StudentPage],
   providers: [
