9e2376d9818709b9cdefdb7369531d789608ccf6
Otavio <otavio.aragoni@gmail.com>
2019-03-19 17:13:56 -0300

    Binding variables and storing data on local storage.

--- src/pages/add-modal/add-modal.ts
+++ src/pages/add-modal/add-modal.ts
@@ -1,11 +1,55 @@
 import { Component } from "@angular/core";
 import { ViewController } from "ionic-angular";
+import { Storage } from "@ionic/storage";
 
 @Component({
   templateUrl: "add-modal.html"
 })
 export class AddModal {
-  constructor(public viewCtrl: ViewController) {}
+  name: string;
+  dateBirth: string;
+  grade: number;
+  zip: number;
+  street: string;
+  number: number;
+  complement: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  motherName: string;
+  motherCPF: number;
+  payDate: number;
+
+  constructor(public viewCtrl: ViewController, private storage: Storage) {}
+
+  async send() {
+    const student = {
+      name: this.name,
+      dateBirth: this.dateBirth,
+      grade: this.grade,
+      zip: this.zip,
+      street: this.street,
+      number: this.number,
+      complement: this.complement,
+      neighborhood: this.neighborhood,
+      city: this.city,
+      state: this.state,
+      motherName: this.motherName,
+      motherCPF: this.motherCPF,
+      payDate: this.payDate
+    };
+
+    const prev = await this.storage.get("students");
+    console.log(prev);
+
+    if (!prev) {
+      await this.storage.set("students", [student]);
+    } else {
+      await this.storage.set("students", [...prev, student]);
+    }
+
+    this.viewCtrl.dismiss();
+  }
 
   dismiss() {
     this.viewCtrl.dismiss();
