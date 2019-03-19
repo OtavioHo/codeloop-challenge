1bd9086a68a5f3797e4714fe95b4736ed277854f
Otavio <otavio.aragoni@gmail.com>
2019-03-19 13:42:39 -0300

    Create page for student view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { StatusBar } from "@ionic-native/status-bar";
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
 import { AddModal } from "../pages/add-modal/add-modal";
+import { StudentPage } from "../pages/student/student";
 
 @NgModule({
-  declarations: [MyApp, HomePage, AddModal],
+  declarations: [MyApp, HomePage, AddModal, StudentPage],
   imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, HomePage, AddModal],
+  entryComponents: [MyApp, HomePage, AddModal, StudentPage],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/pages/student/student.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+@Component({
+  templateUrl: "student.html"
+})
+export class StudentPage {
+  constructor(public navCtrl: NavController) {}
+}
